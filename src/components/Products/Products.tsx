0cfcc7c5cb8a38fd3b3cab315e64df6e8a46f4e8
Abderrahmane Yousfi <bdrrhmnyousfi@gmail.com>
2023-03-13 12:05:58 +0100

    feat: displaying products + overlay-info

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,9 +1,33 @@
+import { useAppSelector } from "../../redux/hooks";
 import styles from "./Products.module.scss";
 
 export const Products = () => {
+  const products = useAppSelector((state) => state.products.products);
+  const isLoading = useAppSelector((state) => state.products.isLoading);
+  const error = useAppSelector((state) => state.products.error);
+
   return (
     <section className={styles.productsContainer}>
       <h2 className={styles.title}>Our Products</h2>
+
+      {isLoading && <h3>Loading ...</h3>}
+      {error && <h3>We've got a problem chief !</h3>}
+
+      {products.length > 0 && (
+        <div className={styles.productsGridContainer}>
+          {products.map((product) => (
+            <div className={styles.card}>
+              <div className={styles.imageContainer}>
+                <img src={product.image} alt="product" />
+              </div>
+              <div className={styles.overlayInfo}>
+                <p className={styles.productTitle}>{product.title}</p>
+                <p>Rate : {product.rating.rate} / 5</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
--- src/redux/products/productsSaga.ts
+++ src/redux/products/productsSaga.ts
@@ -12,7 +12,11 @@ function* getProductsWorker(): any {
     const response = yield call(getProducts);
     if (response.ok) {
       const data = yield response.json();
-      yield put(getProductsSucces(data));
+
+      const shuffledData = data.sort(() => {
+        return Math.random() - 0.5;
+      });
+      yield put(getProductsSucces(shuffledData));
     }
   } catch (error: any) {
     yield put(getProductsFail(error.message));
