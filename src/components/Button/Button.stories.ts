b36f63b71b1de843cbce7cc04ff392a684b18b72
Abderrahmane Yousfi <bdrrhmnyousfi@gmail.com>
2023-05-24 09:30:18 +0100

    feat: button stories

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -4,6 +4,7 @@ import { Button } from "./Button";
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
+
   args: {
     label: "Button",
   },
@@ -12,4 +13,22 @@ export default meta;
 
 type Story = StoryObj<typeof Button>;
 
-export const Primary: Story = {};
+export const Hover: Story = {
+  argTypes: {
+    variant: {
+      control: "inline-radio",
+      options: ["products", "repair", "shop", "nav"],
+    },
+    active: { table: { disable: true } }, // Disable the control for Hover story
+  },
+};
+
+export const Click: Story = {
+  argTypes: {
+    active: { control: "boolean" }, // Enable the control for Click story
+  },
+  args: {
+    variant: "category",
+    active: false,
+  },
+};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,27 @@
 import React from "react";
+import { ReactComponent as NavCart } from "../../assets/svg/nav-cart.svg";
 
 import styles from "./Button.module.scss";
 
 interface ButtonProps {
-  variant: "products" | "repair";
+  variant: string;
   label: string;
+  active?: boolean;
 }
 
 export const Button = ({
   variant = "products",
   label,
+  active,
   ...props
 }: ButtonProps) => {
   return (
-    <button className={styles[variant]} {...props}>
+    <button
+      className={`${styles[variant]} ${active && styles.active}`}
+      {...props}
+    >
       {label}
+      {variant === "nav" && <NavCart fill="white" className={styles.icon} />}
     </button>
   );
 };
